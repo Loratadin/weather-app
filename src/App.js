ca995cc523dd3e9d6a92c4efbedbf260f2014187
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-06-01 20:22:10 -0500

    Create App component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+   return (
+    <div>
+     <h1>What is the weather today...</h1>
+     <p>It's nice! Check it out</p>
+    </div>
+   );
+  }
+};
+
+export default App;
