788382403e84975b270db8dea1c8f9a0b86e2e61
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-06-01 20:34:07 -0500

    Create Form component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ class App extends React.Component {
   render() {
    return (
     <div>
-     <h1>What is the weather today...</h1>
-     <p>It's nice! Check it out</p>
+     <Titles/>
     </div>
    );
   }
---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Form extends React.Component{
+ render() {
+  return(
+   <div>Form component</div>
+  );
+ }
+};
+
+export default Form;
