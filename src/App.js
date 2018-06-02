3966c87282fc47b926832b20bb76d76a2a05c72d
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-06-01 20:52:45 -0500

    Obtain API key

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const API_KEY = "0458137df9115519bc98d8e2925ed1a3";
+
 class App extends React.Component {
   render() {
    return (
