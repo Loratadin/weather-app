2701b3df4ae44286513d841741c067248022aa31
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-06-01 21:03:38 -0500

    Add getWeather function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import Weather from './components/Weather';
 const API_KEY = "0458137df9115519bc98d8e2925ed1a3";
 
 class App extends React.Component {
+  getWeather = async (e) => {
+   const city = e.target.elements.city.value;
+   const country = e.target.elements.country.value;
+   const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+   const data = await api_call.json(); //convert api call to json format and asign it to const data
+  }  
   render() {
    return (
     <div>
