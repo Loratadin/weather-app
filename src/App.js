8414f2edc298be7a1f373ec43f08d5e90e7ef97b
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-06-02 21:14:34 -0500

    Get props for Form

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ const API_KEY = "0458137df9115519bc98d8e2925ed1a3";
 
 class App extends React.Component {
   getWeather = async (e) => {
+    e.preventDefault();
    const city = e.target.elements.city.value;
    const country = e.target.elements.country.value;
-   const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+   const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);//variable contains the function that makes the call to this url
    const data = await api_call.json(); //convert api call to json format and asign it to const data
+   console.log(data)
   }  
   render() {
    return (
     <div>
      <Titles/>
-     <Form/>
+     <Form getWeather={this.getWeather}/>
      <Weather/>
     </div>
    );
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,11 @@ import React from 'react';
 class Form extends React.Component{
  render() {
   return(
-   <div>Form component</div>
+   <form onSubmit={this.props.getWeather}>
+    <input type="text" name="city" placeholder="City..."/>
+    <input type="text" name="country" placeholder="Country..."/>
+    <button>Get weather</button>
+   </form>
   );
  }
 };
