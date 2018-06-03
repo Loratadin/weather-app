1b40e905b3134629925b41c71a1bf396aa657e45
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-06-03 08:48:00 -0500

    Add setState for the App

--- src/App.js
+++ src/App.js
@@ -20,7 +20,15 @@ class App extends React.Component {
    const country = e.target.elements.country.value;
    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);//variable contains the function that makes the call to this url
    const data = await api_call.json(); //convert api call to json format and asign it to const data
-   console.log(data)
+   console.log(data);
+   this.setState({
+    temperature: data.main.temp,
+    city: data.name,
+    country: data.sys.country,
+    humidity: data.main.humidity,
+    description: data.weather[0].description,
+    error: " "
+   });
   }  
   render() {
    return (
