d0dd1530f9fe922629486cea1d99d7de01b60d90
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-06-03 09:39:59 -0500

    Add conditional operator for the Weather component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,10 +4,11 @@ class Weather extends React.Component{
  render() {
   return(
    <div>
-     Location: { this.props.city }, { this.props.country }
-     Temperature: { this.props.temperature }
-     Humidity: { this.props.humidity }
-     Conditions: { this.props.description }
+    {/* if first is true & second is true the third will be shown (true)*/}
+     {this.props.city && this.props.country &&  <p>Location: { this.props.city }, { this.props.country }</p>} 
+     {this.props.temperature && <p>Temperature: { this.props.temperature }</p>}
+     {this.props.humidity && <p>Humidity: { this.props.humidity }</p>}
+     {this.props.description && <p>Conditions: { this.props.description }</p>}
    </div>
   );
  }
