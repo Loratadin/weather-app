329f7015e74586058d2b9715abe35accecb80abb
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-06-03 09:00:36 -0500

    Add props for Weather component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,11 @@ class Weather extends React.Component{
  render() {
   return(
    <div>
-    
+     { this.props.city }
+     { this.props.country }
+     { this.props.temperature }
+     { this.props.humidity }
+     { this.props.description }
    </div>
   );
  }
