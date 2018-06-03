833142752396ebe444a56cde5a5ff8b820039e72
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-06-03 10:17:03 -0500

    Clean up the code for stateless component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Weather = () => {
+const Weather = (props) => {
  return(
   <div>
     {/* conditional operator: if the first is true the second will be shown (true)*/}
-     {this.props.city && this.props.country &&  <p>Location: { this.props.city }, { this.props.country }</p>} 
-     {this.props.temperature && <p>Temperature: { this.props.temperature }</p>}
-     {this.props.humidity && <p>Humidity: { this.props.humidity }</p>}
-     {this.props.description && <p>Conditions: { this.props.description }</p>}
-     {this.props.error && <p>{ this.props.error}</p>}
+     {props.city && props.country &&  <p>Location: { props.city }, { props.country }</p>} 
+     {props.temperature && <p>Temperature: { props.temperature }</p>}
+     {props.humidity && <p>Humidity: { props.humidity }</p>}
+     {props.description && <p>Conditions: { props.description }</p>}
+     {props.error && <p>{ props.error}</p>}
    </div>
  );
 }
