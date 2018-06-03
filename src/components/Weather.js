2cb253277effe5005b7ebb0d0d8a58093edacc2a
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-06-03 09:46:56 -0500

    Add check for city and country to prevent the app from braking

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,7 @@ class Weather extends React.Component{
  render() {
   return(
    <div>
-    {/* if first is true & second is true the third will be shown (true)*/}
+    {/* conditional operator: if the first is true the second will be shown (true)*/}
      {this.props.city && this.props.country &&  <p>Location: { this.props.city }, { this.props.country }</p>} 
      {this.props.temperature && <p>Temperature: { this.props.temperature }</p>}
      {this.props.humidity && <p>Humidity: { this.props.humidity }</p>}
