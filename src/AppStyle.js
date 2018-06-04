fe9d191e5bafc0b48b805b18cfb379e91599eac7
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-06-04 09:42:33 -0500

    Set up the logo icon

---
+++ src/AppStyle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background-image: url(./assets/bg.jpg);
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
